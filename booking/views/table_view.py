15d6a27c51735ab0c5e24479ea9f3aa97eb2d1e9
suwajanee <suwajanee.j@gmail.com>
2018-07-30 16:17:19 +0700

    Fix bug: Delete data

--- booking/views/table_view.py
+++ booking/views/table_view.py
@@ -15,46 +15,27 @@ class BookingTableView(TemplateView):
     
     def get_table(request):
         template_name = 'table.html'
-        if request.method == "POST":
-            form = BookingFilterSortForm(request.POST)
-            date = request.POST.get("date")
-            sort = request.POST.get("sort")
+        if request.method == "GET":
+            form = BookingFilterSortForm(request.GET)
+            date = request.GET.get("date")
 
-            if not date and  not sort:
+            if date == None:
+                date = ''
+
+            if not date:
                 bookings = Booking.objects.order_by('work_id')
-            elif not date:
-                bookings = Booking.objects.order_by('date', sort)
-            elif not sort:
-                bookings = Booking.objects.filter(date=date).order_by('work_id')
             else:
-                bookings = Booking.objects.filter(date=date).order_by('date', sort)
-
-            request.session['form'] = request.POST
-            request.session['date'] = date
-            request.session['sort'] = sort
-        else:
-            form = BookingFilterSortForm()
-            bookings = Booking.objects.order_by('work_id')
-
-            if request.session.has_key('form'):
-                form = BookingFilterSortForm(request.session['form'])
-            if request.session.has_key('date'):
-                date = request.session['date']
-            if request.session.has_key('sort'):
-                sort = request.session['sort']
-
-            if not date and not sort:
-                bookings = Booking.objects.order_by('work_id')
-            elif not date:
-                bookings = Booking.objects.order_by('date', sort)
-            elif not sort:
                 bookings = Booking.objects.filter(date=date).order_by('work_id')
-            else:
-                bookings = Booking.objects.filter(date=date).order_by('date', sort)
 
-        return render(request, template_name, {'bookings': bookings, 'form' : form})
+        return render(request, template_name, {'bookings': bookings, 'form': form, 'date': date})
 
     def delete_data(request, pk):
-        booking = Booking.objects.get(pk = pk)
+        booking = Booking.objects.get(pk=pk)
         booking.delete()
-        return redirect('booking-table')
+
+        if request.method == "GET":
+            date = request.GET.get("date")
+            if not date:
+                return redirect(reverse('booking-table'))
+            else:
+                return redirect(reverse('booking-table') + '?date=' + date)
