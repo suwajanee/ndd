84da9594876a5e6f0ead7bc06ced2e8f373d5416
suwajanee <suwajanee.j@gmail.com>
2018-08-04 17:33:18 +0700

    Table collapse: show more detail (not done)

--- booking/views/edit_table_view.py
+++ booking/views/edit_table_view.py
@@ -8,6 +8,7 @@ from ..forms import BookingFilterSortForm
 # from django.shortcuts import render_to_response
 from datetime import datetime, timedelta
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.contrib import messages
 from django.db.models import Q
 
@@ -38,50 +39,104 @@ class BookingEditTableView(TemplateView):
 
     def save_edit_table(request):
         if request.method == 'POST':
-            pk = dict(request.POST)['pk']
-            time = dict(request.POST)['time']
-            date = dict(request.POST)['date']
-            pickup_tr = dict(request.POST)['pickup_tr']
-            pickup_from = dict(request.POST)['pickup_from']
-            forward_tr = dict(request.POST)['forward_tr']
-            factory = dict(request.POST)['factory']
-            backward_tr = dict(request.POST)['backward_tr']
-            return_tr = dict(request.POST)['return_tr']
-            return_to = dict(request.POST)['return_to']
-            container_no = dict(request.POST)['container_no']
-            seal_no = dict(request.POST)['seal_no']
-            closing_date = dict(request.POST)['closing_date']
-            closing_time = dict(request.POST)['closing_time']
-            ref = dict(request.POST)['ref']
-            remark = dict(request.POST)['remark']
-            return_date = dict(request.POST)['return_date']
+            # pk = dict(request.POST)['pk']
+            # time = dict(request.POST)['time']
+            # date = dict(request.POST)['date']
+            # pickup_tr = dict(request.POST)['pickup_tr']
+            # pickup_from = dict(request.POST)['pickup_from']
+            # forward_tr = dict(request.POST)['forward_tr']
+            # factory = dict(request.POST)['factory']
+            # backward_tr = dict(request.POST)['backward_tr']
+            # return_tr = dict(request.POST)['return_tr']
+            # return_to = dict(request.POST)['return_to']
+            # container_no = dict(request.POST)['container_no']
+            # seal_no = dict(request.POST)['seal_no']
+            # closing_date = dict(request.POST)['closing_date']
+            # closing_time = dict(request.POST)['closing_time']
+            # ref = dict(request.POST)['ref']
+            # remark = dict(request.POST)['remark']
+            # return_date = dict(request.POST)['return_date']
 
-            for i in range(0,len(pk)):
-                if not date[i]:
-                    date[i] = None
-                if not closing_date[i]:
-                    closing_date[i] = None
-                if not return_date[i]:
-                    return_date[i] = None
+            # print(request.POST['date'])
 
-                booking = Booking.objects.get(pk=pk[i])
-                booking.time = time[i]
-                booking.date = date[i]
-                booking.pickup_tr = pickup_tr[i]
-                booking.pickup_from = pickup_from[i]
-                booking.factory = factory[i]
-                booking.backward_tr = backward_tr[i]
-                booking.return_tr = return_tr[i]
-                booking.return_to = return_to[i]
-                booking.container_no = container_no[i]
-                booking.seal_no = seal_no[i]
-                booking.closing_date = closing_date[i]
-                booking.closing_time = closing_time[i]
-                booking.ref = ref[i]
-                booking.remark = remark[i]
-                booking.return_date = return_date[i]
-                booking.save()
-            messages.success(request, "Saved Booking.")
-            return redirect('booking-table')
+            # for i in range(0,len(pk)):
+            #     if not date[i]:
+            #         date[i] = None
+            #     if not closing_date[i]:
+            #         closing_date[i] = None
+            #     if not return_date[i]:
+            #         return_date[i] = None
+
+            #     booking = Booking.objects.get(pk=pk[i])
+            #     booking.time = time[i]
+            #     booking.date = date[i]
+            #     booking.pickup_tr = pickup_tr[i]
+            #     booking.pickup_from = pickup_from[i]
+            #     booking.forward_tr = forward_tr[i]
+            #     booking.factory = factory[i]
+            #     booking.backward_tr = backward_tr[i]
+            #     booking.return_tr = return_tr[i]
+            #     booking.return_to = return_to[i]
+            #     booking.container_no = container_no[i]
+            #     booking.seal_no = seal_no[i]
+            #     booking.closing_date = closing_date[i]
+            #     booking.closing_time = closing_time[i]
+            #     booking.ref = ref[i]
+            #     booking.remark = remark[i]
+            #     booking.return_date = return_date[i]
+            #     booking.save()
+            # messages.success(request, "Saved Booking.")
+
+
+            pk = request.POST['pk']
+            time = request.POST['time']
+            date = request.POST['date']
+            pickup_tr = request.POST['pickup_tr']
+            pickup_from = request.POST['pickup_from']
+            forward_tr = request.POST['forward_tr']
+            factory = request.POST['factory']
+            backward_tr = request.POST['backward_tr']
+            return_tr = request.POST['return_tr']
+            return_to = request.POST['return_to']
+            container_no = request.POST['container_no']
+            seal_no = request.POST['seal_no']
+            closing_date = request.POST['closing_date']
+            closing_time = request.POST['closing_time']
+            ref = request.POST['ref']
+            remark = request.POST['remark']
+            return_date = request.POST['return_date']
+
+            date_filter = request.POST['date_filter']
+
+            if not date:
+                date = None
+            if not closing_date:
+                closing_date = None
+            if not return_date:
+                return_date = None
+
+            booking = Booking.objects.get(pk=pk)
+            booking.time = time
+            booking.date = date
+            booking.pickup_tr = pickup_tr
+            booking.pickup_from = pickup_from
+            booking.forward_tr = forward_tr
+            booking.factory = factory
+            booking.backward_tr = backward_tr
+            booking.return_tr = return_tr
+            booking.return_to = return_to
+            booking.container_no = container_no
+            booking.seal_no = seal_no
+            booking.closing_date = closing_date
+            booking.closing_time = closing_time
+            booking.ref = ref
+            booking.remark = remark
+            booking.pickup_date = date
+            booking.factory_date = date
+            booking.return_date = return_date
+            booking.save()
+
+            messages.success(request, "Saving Booking.")
+            return redirect(reverse('booking-edit') + '?date=' + date_filter)
         else:
             return redirect('booking-edit')
