01378b5f2c897127f4f3b8f68de834954be43791
hoikhong <57010993@kmitl.ac.th>
2018-09-05 11:19:03 +0700

    refactor code  agent_transport_update_view, agent_transport_table_view

--- ndd-app/agent_transport/urls.py
+++ ndd-app/agent_transport/urls.py
@@ -3,12 +3,16 @@ from django.conf.urls import url
 from .views.agent_transport_table_view import AgentTransportTableView
 from .views.agent_transport_add_view import AgentTransportAddView
 from .views.agent_transport_edit_table_view import AgentTransportEditTableView
+from .views.agent_transport_update_view import AgentTransportUpdateView
 from .views.agent_transport_print_view import AgentTransportPrintView
 from .views.agent_transport_delete_view import AgentTransportDeleteView
 
 
 urlpatterns = [
-    url(r'^$', AgentTransportTableView.get_table, name='agent-transport-table'),
+    url(r'^$', AgentTransportTableView.render_table_agent_transport, name='agent-transport-table'),
+    url(r'^update/$', AgentTransportUpdateView.update_data_agent_transport, name='agent-transport-update'), #update in table page
+
+
     url(r'^print/(?P<pk>\d+)/(?P<template>\w+)/$', AgentTransportPrintView.as_view(), name='agent-transport-print'),
 
     url(r'^add/$', AgentTransportAddView.add_agent_transport, name='agent-transport-add'),
@@ -16,8 +20,6 @@ urlpatterns = [
 
     url(r'^delete/(?P<pk>\d+)/$', AgentTransportDeleteView.delete_data, name='agent-transport-delete'), #delete in table page
     url(r'^delete/$', AgentTransportDeleteView.delete_multiple, name='agent-transport-delete-multiple'),
-
-    url(r'^update/$', AgentTransportTableView.update_data, name='agent-transport-update'), #update in table page
 
     url(r'^edit/$', AgentTransportEditTableView.get_edit_table, name='agent-transport-edit'),
     url(r'^edit/save$', AgentTransportEditTableView.save_edit_table, name='agent-transport-edit-save'),
--- ndd-app/agent_transport/views/agent_transport_table_view.py
+++ ndd-app/agent_transport/views/agent_transport_table_view.py
@@ -12,53 +12,30 @@ from django.contrib.auth.decorators import login_required
 class AgentTransportTableView(TemplateView):
 
     @login_required(login_url=reverse_lazy('login'))
-    def get_table(request):
+    def render_table_agent_transport(request):
         template_name = 'agent_transport/agent_transport_table.html'
 
         today = datetime.now()
 
         if request.method == "GET":
             filter_by = request.GET.get("filter_by")
-            date = request.GET.get("date")
+            date_filter = request.GET.get("date_filter")
 
-            if date == None:
-                date = ''
+            if date_filter == None:
+                date_filter = ''
 
-            if not date:
+            if not date_filter:
                 agent_transports = AgentTransport.objects.filter((Q(date__month=today.month) & Q(date__year=today.year)) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
             else:
                 if filter_by == "month":
-                    month_year = datetime.strptime(date, '%Y-%m')
-                    agent_transports = AgentTransport.objects.filter((Q(date__month=month_year.month) & Q(date__year=month_year.year)) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
+                    month_of_year = datetime.strptime(date_filter, '%Y-%m')
+                    agent_transports = AgentTransport.objects.filter((Q(date__month=month_of_year.month) & Q(date__year=month_of_year.year)) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
                 else:
-                    agent_transports = AgentTransport.objects.filter(Q(date=date) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
+                    agent_transports = AgentTransport.objects.filter(Q(date=date_filter) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
         else:
             agent_transports = AgentTransport.objects.filter((Q(date__month=today.month) & Q(date__year=today.year)) | (Q(return_tr='') & ~Q(cancel='1'))).order_by('date', 'work_id')
 
-        return render(request, template_name, {'agent_transports': agent_transports, 'filter_by': filter_by, 'date': date, 'today': today, 'nbar': 'agent-transport-table'})
+        return render(request, template_name, {'agent_transports': agent_transports, 'filter_by': filter_by, 'date_filter': date_filter, 'today': today, 'nbar': 'agent-transport-table'})
     
 
-    @login_required(login_url=reverse_lazy('login'))
-    def update_data(request):
-        if request.method == 'POST':
-            pk = request.POST['pk']
-            address = request.POST['address'+pk]
-            if address == 'other':
-                address_other = request.POST['address_other']
-
-            cancel = request.POST['cancel']
-
-            filter_by = request.POST['filter_by']
-            date_filter = request.POST['date_filter']
-
-            agent_transport = AgentTransport.objects.get(pk=pk)
-            agent_transport.address = address
-            if address == 'other':
-                agent_transport.address_other = address_other
-            agent_transport.cancel = cancel
-            agent_transport.save()
-
-            messages.success(request, "Updated Agent Transport.")
-            return redirect(reverse('agent-transport-table') + '?filter_by=' + filter_by + '&date=' + date_filter)
-        else:
-            return redirect('agent-transport-table')
+   
