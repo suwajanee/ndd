fd501dd3952b089e53cb1495007b133b7e18b015
suwajanee <suwajanee.j@gmail.com>
2018-09-06 11:44:48 +0700

    Refactor edit customer view / Remove unused part

--- ndd-app/customer/views/customer_delete_view.py
+++ ndd-app/customer/views/customer_delete_view.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView
