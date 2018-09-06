f44ec1818ae2c9c9686a2c5e11ab8586ccf7b976
suwajanee <suwajanee.j@gmail.com>
2018-09-06 15:09:38 +0700

    Add cancel fields for principal and shipper / Remove booking color field in agent-transport

--- ndd-app/customer/admin.py
+++ ndd-app/customer/admin.py
@@ -8,11 +8,11 @@ class ShipperForm(forms.ModelForm):
     address = forms.CharField(widget=forms.Textarea)
 
 class PrincipalAdmin(admin.ModelAdmin):
-    list_display = ('name', 'work_type')
+    list_display = ('name', 'work_type', 'cancel')
 
 class ShipperAdmin(admin.ModelAdmin):
     form = ShipperForm
-    list_display = ('principal', 'name', 'address')
+    list_display = ('principal', 'name', 'address', 'cancel')
 
 admin.site.register(Principal, PrincipalAdmin)
 admin.site.register(Shipper, ShipperAdmin)
