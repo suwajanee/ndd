9826317dd38f63b80cabc2aa052589065903c266
hoikhong <57010993@kmitl.ac.th>
2018-09-12 11:01:37 +0700

    Fixed Add Container style booking/agent first in

--- ndd-app/static/js/agent_transport_add.js
+++ ndd-app/static/js/agent_transport_add.js
@@ -1,29 +1,27 @@
-$(document).ready(function () {
+$(function () {
 
-    window.setTimeout(function () {
-        $(".alert").fadeTo(500, 0).slideUp(500, function () {
-            $(this).remove();
+    window.setTimeout(function() {
+        $(".alert").fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove(); 
         });
     }, 2000);
-
-});
-
-$(document).on('click', '.add-more', function (e) {
-    e.preventDefault();
-
-    var controlForm = $('.control:first'),
-        currentEntry = $(this).parents('.entry:first'),
-        newEntry = $(currentEntry.clone()).appendTo(controlForm);
-
-    newEntry.find('input').val('');
-    controlForm.find('.entry:not(:last) .add-more')
-        .removeClass('add-more').addClass('remove')
-        .removeClass('btn-success').addClass('btn-danger')
-        .html('<span><i class="fa fa-minus"></i></span>');
-}).on('click', '.remove', function (e) {
-    $(this).parents('.entry:first').remove();
-
-    e.preventDefault();
-    return false;
+    
+    
+        $(document).on('click', '.add-more', function(e) {
+            e.preventDefault();
+            var controlForm = $('.control:first'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+            newEntry.find('input').val('');
+            controlForm.find('.entry:not(:first) .add-more')
+            .removeClass('add-more').addClass('remove')
+            .removeClass('btn-success').addClass('btn-danger')
+            .html('<span><i class="fa fa-minus"></i></span>');
+        }).on('click', '.remove', function(e) {
+            $(this).parents('.entry:first').remove();
+            e.preventDefault();
+            return false;
+        });
+    
 
 });
