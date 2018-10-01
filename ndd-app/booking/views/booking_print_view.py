b0221739299ac250a0e96aac62d12f36f7736ccf
suwajanee <suwajanee.j@gmail.com>
2018-10-01 11:31:19 +0700

    Add session for print template

--- ndd-app/booking/views/booking_print_view.py
+++ ndd-app/booking/views/booking_print_view.py
@@ -16,40 +16,71 @@ from ndd.settings import STATICFILES_DIRS
 
 
 class BookingPrintView(TemplateView):
+
+    def get(self, request, pk):
+        context = {}
+        context['static_dir'] = STATICFILES_DIRS[0]
+        context['booking'] = get_object_or_404(Booking, pk=pk)
+
+        if request.session['template_name']:               
+            template_name = request.session['template_name']
+            address = request.session['address']
+
+            if address == 'other':
+                context['address'] = request.session['address_other']
+                request.session['address_other'] = context['address']
+            elif address == 'shipper':
+                try:
+                    shipper = Shipper.objects.get(Q(principal=context['booking'].principal) & Q(name=context['booking'].shipper))
+                    context['address'] = shipper.address
+                except Shipper.DoesNotExist:
+                    context['address'] = ''
+            else:
+                context['address'] = ''
+
+            request.session['template_name'] = template_name
+            request.session['address'] = address
+
+        return self.render(template_name, context)
     
     def post(self, request, pk):
-        template = request.POST["template"]
-        address = request.POST["address"+pk]
-
-        if template == 'forward':
-            template_name = 'pdf_template/booking_fw_template.html'
-        elif template == 'backward':
-            template_name = 'pdf_template/booking_bw_template.html'
-        elif template == 'yard_ndd':
-            template_name = 'pdf_template/booking_yard_ndd_template.html'
-        elif template == 'ndd_fac_return':
-            template_name = 'pdf_template/booking_ndd_fac_return_template.html'
-        elif template == 'ndd_fac_ndd':
-            template_name = 'pdf_template/booking_ndd_fac_ndd_template.html'
-        elif template == 'ndd_return':
-            template_name = 'pdf_template/booking_ndd_return_template.html'
-        else:
-            template_name = 'pdf_template/booking_full_template.html'
-            
         context = {}
-        context['booking'] = get_object_or_404(Booking, pk=pk)
         context['static_dir'] = STATICFILES_DIRS[0]
+        context['booking'] = get_object_or_404(Booking, pk=pk)
 
-        if address == 'other':
-            context['address'] = request.POST["address_other"]
-        elif address == 'shipper':
-            try:
-                shipper = Shipper.objects.get(Q(principal=context['booking'].principal) & Q(name=context['booking'].shipper))
-                context['address'] = shipper.address
-            except Shipper.DoesNotExist:
+        if request.method == "POST":
+            template = request.POST["template"]
+            address = request.POST["address"+pk]
+            
+            if template == 'forward':
+                template_name = 'pdf_template/booking_fw_template.html'
+            elif template == 'backward':
+                template_name = 'pdf_template/booking_bw_template.html'
+            elif template == 'yard_ndd':
+                template_name = 'pdf_template/booking_yard_ndd_template.html'
+            elif template == 'ndd_fac_return':
+                template_name = 'pdf_template/booking_ndd_fac_return_template.html'
+            elif template == 'ndd_fac_ndd':
+                template_name = 'pdf_template/booking_ndd_fac_ndd_template.html'
+            elif template == 'ndd_return':
+                template_name = 'pdf_template/booking_ndd_return_template.html'
+            else:
+                template_name = 'pdf_template/booking_full_template.html'
+            
+            if address == 'other':
+                context['address'] = request.POST["address_other"]
+                request.session['address_other'] = context['address']
+            elif address == 'shipper':
+                try:
+                    shipper = Shipper.objects.get(Q(principal=context['booking'].principal) & Q(name=context['booking'].shipper))
+                    context['address'] = shipper.address
+                except Shipper.DoesNotExist:
+                    context['address'] = ''
+            else:
                 context['address'] = ''
-        else:
-            context['address'] = ''
+
+        request.session['template_name'] = template_name
+        request.session['address'] = address
 
         return self.render(template_name, context)
 
