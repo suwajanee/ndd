36c38b36ae503ea9cbddcdd3b127540252d07a65
suwajanee <suwajanee.j@gmail.com>
2018-10-04 19:13:34 +0700

    Change condition to show bookings

--- ndd-app/booking/views/booking_edit_view.py
+++ ndd-app/booking/views/booking_edit_view.py
@@ -32,7 +32,7 @@ class BookingEditTableView(TemplateView):
                 context['date_filter'] = ''
 
             if not context['date_filter']:
-                context['bookings'] = Booking.objects.filter((Q(date__month=context['today'].month) & Q(date__year=context['today'].year)) | Q(status=1)).order_by('date', 'principal__name', 'shipper__name', 'work_id')
+                context['bookings'] = Booking.objects.filter(Q(date=context['today']) | Q(status=1)).order_by('date', 'principal__name', 'shipper__name', 'work_id')
             else:
                 if context['filter_by'] == "month":
                     month_of_year = datetime.strptime(context['date_filter'], '%Y-%m')
@@ -41,7 +41,7 @@ class BookingEditTableView(TemplateView):
                     context['bookings'] = Booking.objects.filter(Q(date=context['date_filter']) | ((Q(closing_date__lte=context['tmr']) | Q(date__lte=context['today'])) & Q(status=1))).order_by('date', 'principal__name', 'shipper__name', 'work_id')
 
         else:
-            context['bookings'] = Booking.objects.filter((Q(date__month=context['today'].month) & Q(date__year=context['today'].year)) | Q(status=1)).order_by('date', 'principal__name', 'shipper__name', 'work_id')
+            context['bookings'] = Booking.objects.filter(Q(date=context['today']) | Q(status=1)).order_by('date', 'principal__name', 'shipper__name', 'work_id')
 
         return render(request, template_name, context)        
 
