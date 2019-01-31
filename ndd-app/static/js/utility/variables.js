38a33979a2127965ea69b5e7e066b2020dde03b6
suwajanee <suwajanee.j@gmail.com>
2019-01-31 11:47:45 +0700

    Fix choice form setting

--- ndd-app/static/js/utility/variables.js
+++ ndd-app/static/js/utility/variables.js
@@ -2,7 +2,6 @@ const booking_field_text = {
     '00': 'BOOKING',
     '01': 'B/L',
     '02': 'ROUTING',
-    '03': 'JOB NO/BOOKING',
 }
 
 const _month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
