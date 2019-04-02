48a76f8b24cb3cbf3e72fc9cf2ef83a1290a1d7b
suwajanee <suwajanee.j@gmail.com>
2019-04-02 18:02:43 +0700

    Delete invoice detail when delete work / Sum total after delete invoice detail

--- ndd-app/booking/views/booking_delete_view.py
+++ ndd-app/booking/views/booking_delete_view.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import decimal
 import json
 
 from django.http import JsonResponse
@@ -7,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from ..models import Booking
 from .booking_page_view import api_filter_bookings
+from summary.models import InvoiceDetail
 
 
 @csrf_exempt
@@ -18,6 +20,21 @@ def api_delete_bookings(request):
 
             for pk in pk_list:
                 booking = Booking.objects.get(pk=pk)
+
+                details = InvoiceDetail.objects.filter(work_normal=booking)
+                for detail in details:
+                    if detail.drayage_charge['drayage']:
+                        detail.invoice.drayage_total -= decimal.Decimal(eval(detail.drayage_charge['drayage']))
+                    if 'other' in detail.drayage_charge:
+                        for other in detail.drayage_charge['other']:
+                            if other['other_charge']:
+                                detail.invoice.drayage_total -= decimal.Decimal(eval(other['other_charge']))
+                    if  detail.gate_charge:
+                        if detail.gate_charge['gate']:
+                            detail.invoice.gate_total -= decimal.Decimal(eval(detail.gate_charge['gate']))
+
+                    detail.invoice.save()
+                    detail.delete()
                 booking.delete()
 
         return api_filter_bookings(request)
