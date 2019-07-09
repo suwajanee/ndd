b1c949c369cba14b2d45e1ef9296cffa57b1ecb2
suwajanee <suwajanee.j@gmail.com>
2019-07-09 17:12:41 +0700

    Employee in admin page / set __str__ for data

--- ndd-app/employee/admin.py
+++ ndd-app/employee/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Driver
+from .models import Employee
+from .models import Job
+from .models import Salary
 
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('job_title',)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date', 'hire_date', 'job', 'detail', 'status')
+    ordering = ('first_name', 'last_name')
+    search_fields = ['first_name', 'last_name']
+
+
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'license_type', 'pat_pass_expired_date')
+    ordering = ('employee__first_name', 'employee__last_name')
+
+
+class SalaryAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'salary', 'from_date', 'to_date')
+    ordering = ('employee__first_name', 'employee__last_name')
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Driver, DriverAdmin)
+admin.site.register(Salary, SalaryAdmin)
--- ndd-app/employee/models.py
+++ ndd-app/employee/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Job(models.Model):
     job_title = models.CharField(max_length=15, blank=True, null=True, default='')
 
+    def __str__(self) :
+        return self.job_title
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=50, blank=True, null=True, default='')
@@ -16,11 +19,17 @@ class Employee(models.Model):
     job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True, blank=True)
     detail = JSONField(null=True, blank=True, default=dict)
 
+    def __str__(self) :
+        return self.first_name + ' ' + self.last_name
+
 
 class Driver(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
     license_type = models.CharField(max_length=1, blank=True, null=True, default='')
     pat_pass_expired_date = models.DateField(blank=True, null=True, default=None)
+
+    def __str__(self) :
+        return self.employee.first_name + ' ' + self.employee.last_name
 
 
 class Salary(models.Model):
@@ -28,3 +37,6 @@ class Salary(models.Model):
     salary = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     from_date = models.DateField(blank=True, null=True, default=None)
     to_date = models.DateField(blank=True, null=True, default=None)
+
+    def __str__(self) :
+        return self.employee.first_name + ' ' + self.employee.last_name
