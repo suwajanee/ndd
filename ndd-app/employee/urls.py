316fe7cd469aad8b6c94d780c8bfc47d7dd6f97d
suwajanee <suwajanee.j@gmail.com>
2019-07-18 11:19:18 +0700

    API get employee data

--- ndd-app/employee/urls.py
+++ ndd-app/employee/urls.py
@@ -6,5 +6,6 @@ from .views import employee_page_view
 urlpatterns = [
 
     url(r'^$', employee_page_view.employee_page, name='employee-page'),
+    url(r'^api/get-employee/$', employee_page_view.api_get_employee, name='api-get-employee'),
 
 ]
--- ndd-app/employee/views/employee_page_view.py
+++ ndd-app/employee/views/employee_page_view.py
@@ -4,7 +4,23 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
+from ..models import Employee
+from ..serializers import EmployeeSerializer
+
 
 @login_required(login_url=reverse_lazy('login'))
 def employee_page(request):
     return render(request, 'employee/employee_page.html', {'nbar': 'database-page'})
+
+@csrf_exempt
+def api_get_employee(request):
+    if request.user.is_authenticated:
+        if request.method == "GET":
+            req = json.loads( request.body.decode('utf-8') )
+
+            employee = Employee.objects.all().order_by('cancel', 'name')
+
+        serializer = EmployeeSerializer(employee, many=True)
+
+        return JsonResponse(serializer.data, safe=False)
+    return JsonResponse('Error', safe=False) 
