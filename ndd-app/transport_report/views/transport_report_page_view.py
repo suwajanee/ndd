6cb9842ed79797a3a7b6b3bfd534c210531b0a88
suwajanee <suwajanee.j@gmail.com>
2020-02-19 17:58:27 +0700

    Separate expense field to new file

--- ndd-app/transport_report/views/transport_report_page_view.py
+++ ndd-app/transport_report/views/transport_report_page_view.py
@@ -9,13 +9,13 @@ from employee.models import Employee
 
 @login_required(login_url=reverse_lazy('login'))
 def expense_page(request):
-    return render(request, 'transport_report/transport_report_expense.html', {'nbar': 'transport-report-page', 'co': 'ndd'})
+    return render(request, 'transport_report/transport_report_daily_expense_page.html', {'nbar': 'transport-report-page', 'co': 'ndd'})
 
 @login_required(login_url=reverse_lazy('login'))
 def date_expense_page(request, date, co):
-    return render(request, 'transport_report/transport_report_expense.html', {'nbar': 'transport-report-page', 'date': date, 'co': co})
+    return render(request, 'transport_report/transport_report_daily_expense_page.html', {'nbar': 'transport-report-page', 'date': date, 'co': co})
 
 @login_required(login_url=reverse_lazy('login'))
 def driver_expense_page(request, date, driver):
     co = Employee.objects.get(pk=driver).co
-    return render(request, 'transport_report/transport_report_expense.html', {'nbar': 'transport-report-page', 'date': date, 'co': co, 'driver': driver})
+    return render(request, 'transport_report/transport_report_daily_expense_page.html', {'nbar': 'transport-report-page', 'date': date, 'co': co, 'driver': driver})
