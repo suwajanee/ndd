0a4a7cad2b30e62b1a0c1cb0a972c338856d3115
suwajanee <suwajanee.j@gmail.com>
2020-07-13 15:33:44 +0700

    Fix format number in summary report PDF

--- ndd-app/summary/templatetags/result.py
+++ ndd-app/summary/templatetags/result.py
@@ -8,17 +8,11 @@ def result(value):
         value = str(value)
         result = eval(value)
 
-        result = "{:,}".format(result)
+        result = "{:,.2f}".format(result)
 
         result = str(result)
-        x = result.split('.')
 
-        if len(x) == 1:
-            x.append('00')
-        elif len(x[1]) == 1:
-            x[1] = x[1] + '0'
-
-        return x[0] + '.' + x[1]
+        return result
     return '-'
 
 @register.filter(name='add_float')
